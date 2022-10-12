0e20d293b60d0572a26b2ef91d8393ba252ea977
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-10-12 10:26:57 -0300

    Desafio concluídogit add .! Implementei também um tratamento de erros.

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -14,54 +14,72 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            string veiculo = Console.ReadLine();
+            this.veiculos.Add(veiculo);
         }
 
         public void RemoverVeiculo()
         {
-            Console.WriteLine("Digite a placa do veículo para remover:");
-
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            string placa = "";
-
-            // Verifica se o veículo existe
-            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            try
             {
-                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                Console.WriteLine("Digite a placa do veículo para remover:");
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
+                string placa = Console.ReadLine();
 
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
+                // Verifica se o veículo existe
+                if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+                {
+                    Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                    int horas = int.Parse(Console.ReadLine());
+                    decimal valorTotal = this.precoInicial + this.precoPorHora * horas;
+
+                    this.veiculos.Remove(placa);
+
+                    Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                }
+                else
+                {
+                    Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+                }
             }
-            else
+            catch (Exception e)
             {
-                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+                Console.WriteLine("Ocorreu um erro.");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Aperte qualquer tecla para continuar mesmo assim.");
+                Console.ReadLine();
             }
         }
 
         public void ListarVeiculos()
         {
-            // Verifica se há veículos no estacionamento
-            if (veiculos.Any())
+            try
             {
-                Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                // Verifica se há veículos no estacionamento
+                if (veiculos.Any())
+                {
+                    Console.WriteLine("Os veículos estacionados são:");
+
+                    for (int i = 0; i < this.veiculos.Count; i++)
+                    {
+                        Console.WriteLine(this.veiculos[i]);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Não há veículos estacionados.");
+                }
             }
-            else
+            catch (Exception e)
             {
-                Console.WriteLine("Não há veículos estacionados.");
+                Console.WriteLine("Ocorreu um erro.");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Aperte qualquer tecla para continuar mesmo assim.");
+                Console.ReadLine();
             }
+
         }
     }
 }
